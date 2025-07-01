27093b6879564b9eb8862e8dcf207b86399b9671
victoria-mckinney <victoria.mckinney@digital.cabinet-office.gov.uk>
2025-07-01 14:21:44 +0100

    removed non-python docstrings from __utils__.py in notation_generator

--- notation_generator/__init__.py
+++ notation_generator/__init__.py
@@ -1,7 +1,1 @@
 # DrumScript/notation_generator/__init__.py
-
-# This file makes the 'notation_generator' directory a Python package.
-# You can optionally import frequently used components here to simplify imports
-# for users of this package, e.g.:
-# from .score_builder import quantize_events, create_score_data
-# from .pdf_exporter import generate_pdf
--- notation_generator/score_builder.py
+++ notation_generator/score_builder.py
@@ -4,7 +4,7 @@ from typing import List, Dict, Any
 #from . import config
 #from . import constants
 #from .helpers import seconds_to_beats, round_to_nearest_subdivision, format_event_for_notation_library
-import config
+import utils.config as config
 import constants
 from helpers import seconds_to_beats, round_to_nearest_subdivision, format_event_for_notation_library
 
